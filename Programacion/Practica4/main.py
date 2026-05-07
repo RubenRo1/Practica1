978bcda9c3b451ffd4e65b5a4602c74f19f8c44e
Ruben <rubenrodriguezcatrufo@gmail.com>
2026-05-07 12:26:46 +0200

    mostrar datos

--- Programacion/Practica4/main.py
+++ Programacion/Practica4/main.py
@@ -1,40 +1,38 @@
 from producto import producto
 from avl_tree import AVL
+import csv
 
 
-def prueba_arbol():
-    avl = AVL()
-
-    p1 = producto("8410000000011", "Leche Entera 1L", "Lacteos", 1.25, 100, ["Norte", "PastoSA"])
-    p2 = producto("8412345678901", "Arroz Largo 1kg", "Cereales", 2.15, 50, ["Arrocera Valencia"])
-    p3 = producto("8422222222222", "Aceite Oliva 1L", "Aceites", 5.99, 30, ["Olivar"])
-
-    avl[p1.codigo_barra] = p1
-    avl[p2.codigo_barra] = p2
-    avl[p3.codigo_barra] = p3
-
-    inorden(avl, avl.root())
-
-def inorden(tree, p):
-
-
-    print(p.key() ," -> ", p.value().nombre)
-    p2 = tree.left(p)
-    print(p2.key() ," -> ", p2.value().nombre)
-    
-
-    # if p is not None:
-    #     inorden(tree, tree.left(p))
-    #     print(p.key(), "->", p.value().nombre)
-    #     inorden(tree, tree.right(p))
+avl_super = AVL()
+avl_max = AVL()
 
 
 def cargar_datos():
-    pass
+    with open("Practica4/inventario_supercompra.csv",newline="") as csvfile:
+        producto_supermercado = csv.reader(csvfile,delimiter=",")
 
+        for row in producto_supermercado:
+            producto_supercompra = producto(row[0],row[1],row[2],row[3],row[4],row[5],row[6])
+            avl_super[producto_supercompra.codigo_barra] = producto_supercompra
+
+    with open("Practica4/inventario_mercamax.csv",newline="") as csvfile:
+        producto_supermercado2 = csv.reader(csvfile,delimiter=",")
+
+        for row in producto_supermercado2:
+            producto_mercamax = producto(row[0],row[1],row[2],row[3],row[4],row[5],row[6])
+            avl_max[producto_mercamax.codigo_barra] = producto_mercamax
+
+
+def mostrar_datos(tree,avl_super):
+     
+     if avl_super is not None:
+        mostrar_datos(tree, tree.left(avl_super))
+        print(f"Codigo: {avl_super.key()} -> {avl_super.value()}")
+        mostrar_datos(tree, tree.right(avl_super))
 
 def unificar():
     pass
 
 if __name__ == "__main__":
-    prueba_arbol()
+    cargar_datos()
+    mostrar_datos(avl_super, avl_super.root())
--- Programacion/Practica4/producto.py
+++ Programacion/Practica4/producto.py
@@ -1,13 +1,15 @@
 
 
 class producto:
-    def __init__(self, codigo_barra, nombre, categoria, precio, stock, proveedores:list):
+    def __init__(self, codigo_barra, nombre, categoria, precio, stock, proveedores:list, fecha):
         self.codigo_barra = codigo_barra
         self.nombre = nombre
         self.categoria = categoria
         self.precio = precio
         self.stock = stock
         self.proveedores = proveedores
+        self.fecha = fecha
 
     
-
+    def __str__(self):
+        return f"{self.nombre} | {self.categoria} | {self.precio} | {self.stock} | {self.proveedores} | {self.fecha} |"
